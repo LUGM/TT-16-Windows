04201971eaf889265e820bd4b26f91fafc6cfa17
demonic-bling <gauthamvinod@outlook.com>
2016-06-18 15:43:48 +0530

    Basic UI
    v1.0.5

---
+++ Tech Tatva '16/Tech Tatva '16/Classes/EventClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tech_Tatva__16
+{
+    class EventClass
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
